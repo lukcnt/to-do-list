a3ce0458e6b5e75bac48414112c0ccd8c29c3d8c
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-04 22:51:48 -0300

    Create a new task functionality

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const addButton = document.querySelector("#add-button");
+const taskTitle = document.querySelector("#task-title");
+const template = document.querySelector(".template");
+const list = document.querySelector("#task-list");
+
+function addTask() {
+    let task = taskTitle.value;
+    
+    if (task) {
+        let newTask = template.cloneNode(true);
+        newTask.querySelector(".task-title").textContent = task;
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");
+        list.appendChild(newTask);
+        document.querySelector("#task-title").value = "";
+    }
+}
+
+addButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    addTask();
+});
